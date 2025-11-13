830b8f338d8a2ed65293c1b9f256f4e14d710dd1
21_MORT <opermion.mel@gmail.com>
2025-11-13 09:12:17 +0000

    sample program to test

--- src/lib/x86/sample-programs.ts
+++ src/lib/x86/sample-programs.ts
@@ -39,5 +39,45 @@ exit:
   mov eax, 1
   int 0x80
 `
+  },
+  {
+    name: "factorial.asm",
+    code: `; Recursive factorial calculation
+section .text
+  global _start
+
+_start:
+  mov eax, 4      ; Calculate factorial of 4
+  push eax
+  call factorial
+  add esp, 4      ; Clean up stack after call
+
+  ; The result is in EAX
+  mov ebx, eax    ; Exit code is the result
+  mov eax, 1
+  int 0x80
+
+factorial:
+  push ebp        ; Set up stack frame
+  mov ebp, esp
+
+  mov eax, [ebp+8] ; Get argument (n)
+
+  cmp eax, 1
+  jle end_factorial ; If n <= 1, return 1
+
+  dec eax
+  push eax
+  call factorial  ; factorial(n-1)
+  add esp, 4      ; Clean up stack
+
+  mov ebx, [ebp+8] ; Get n again
+  mul ebx         ; EAX = EAX * EBX (n * (n-1)!)
+
+end_factorial:
+  mov esp, ebp    ; Tear down stack frame
+  pop ebp
+  ret
+`
   }
 ];
