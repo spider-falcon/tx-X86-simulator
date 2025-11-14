e3e13f8823d80a9ec6f5999b2449ef1088c81fe3
21_MORT <opermion.mel@gmail.com>
2025-11-14 13:33:51 +0000

    /** @type {import('next').NextConfig} */ const nextConfig = {   output:

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type {NextConfig} from 'next';
 
 const isProd = process.env.NODE_ENV === 'production';
-const repoName = 'tx-X86-simulator'; // <-- IMPORTANT: Replace this with your GitHub repository name
+const repoName = 'tx-X86-simulator';
 
 const nextConfig: NextConfig = {
   /* config options here */
